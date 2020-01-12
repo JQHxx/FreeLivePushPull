79f5c3f278c7e328a78f4482ea8946b54d8d3439
huangjiaqian <18775134221@163.com>
2020-01-12 16:13:20 +0800

    提交

---
+++ PLPullPlayer/PLPullPlayer/Class/LivePush/NewKSYLivePushView.h
@@ -0,0 +1,45 @@
+//
+//  NewKSYLivePushView.h
+//  KSYLivePush
+//
+//  Created by OFweek01 on 2020/1/7.
+//  Copyright © 2020 OFweek01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <libksygpulive/KSYGPUStreamerKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol NewKSYLivePushViewDelegate <NSObject>
+
+// 根据获取按钮title区分不同的状态
+- (void)newKSYLivePushViewStartButtonClicked;
+
+- (void)newKSYLivePushViewExitLive;
+
+@end
+
+@class SpreadButton;
+@interface NewKSYLivePushView : UIView
+
+#pragma mark - kit instance
+
+@property (weak, nonatomic) id<NewKSYLivePushViewDelegate> delegate;
+
+- (instancetype)initWithProtrait;
+
+- (instancetype)initWithLandscape;
+
+- (void)startStream:(NSString *)streamUrl;
+
+- (void)endStream;
+
+/**
+ 直播开始按钮
+ */
+@property (strong, nonatomic) SpreadButton *streamStartButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
